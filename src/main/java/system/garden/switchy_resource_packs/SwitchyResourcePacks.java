d1af619c3b4787b93fbcb4d27a12daa3d66e35a5
HestiMae <hestiaheartstrings@gmail.com>
2023-02-13 15:29:10 +1100

    Initial commit

---
+++ src/main/java/system/garden/switchy_resource_packs/SwitchyResourcePacks.java
@@ -0,0 +1,18 @@
+package system.garden.switchy_resource_packs;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.QuiltLoader;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SwitchyResourcePacks implements ClientModInitializer {
+	public static final String ID = "switchy_resource_packs";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+
+	}
+}
