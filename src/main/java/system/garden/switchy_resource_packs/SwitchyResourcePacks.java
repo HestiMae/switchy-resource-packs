90a6a62dc711a79c5aa3810a77ce9b2bde468342
HestiMae <hestiaheartstrings@gmail.com>
2023-02-14 14:55:15 +1100

    First working version

--- src/main/java/system/garden/switchy_resource_packs/SwitchyResourcePacks.java
+++ src/main/java/system/garden/switchy_resource_packs/SwitchyResourcePacks.java
@@ -1,24 +1,44 @@
 package system.garden.switchy_resource_packs;
 
+import folk.sisby.switchy.client.api.SwitchyEventsClient;
 import net.minecraft.client.MinecraftClient;
-import org.quiltmc.config.api.values.ValueList;
+import net.minecraft.client.util.Session;
+import net.minecraft.resource.pack.ResourcePackManager;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.loader.api.config.QuiltConfig;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
-import org.quiltmc.qsl.resource.loader.api.client.ClientResourceLoaderEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class SwitchyResourcePacks implements ClientModInitializer {
 	public static final String ID = "switchy_resource_packs";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
-	public static final SwitchyResourcePacksConfig CONFIG = QuiltConfig.create(ID, "packs", SwitchyResourcePacksConfig.class);
+	public static final SwitchyResourcePacksConfig CONFIG = QuiltConfig.create(ID, "config", SwitchyResourcePacksConfig.class);
 
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-		CONFIG.presetPacks.put("default", ValueList.create("bean"));
-		ClientResourceLoaderEvents.END_RESOURCE_PACK_RELOAD.register((client, resourceManager, first, error)->{
-			CONFIG.presetPacks.get("default").addAll(MinecraftClient.getInstance().getResourcePackManager().getEnabledNames());
+		SwitchyEventsClient.registerSwitchListener(new Identifier(ID, "reload_listener"), (switchEvent)-> {
+			Session session = MinecraftClient.getInstance().getSession();
+			if (CONFIG.enabled && session.getPlayerUuid() != null && switchEvent.player.equals(session.getPlayerUuid()))
+			{
+				List<String> resourcePacks = MinecraftClient.getInstance().options.resourcePacks;
+				ResourcePackManager resourcePackManager = MinecraftClient.getInstance().getResourcePackManager();
+				if (switchEvent.previousPreset != null)
+				{
+					CONFIG.presetPacks.putIfAbsent(switchEvent.previousPreset, CONFIG.presetPacks.getDefaultValue());
+					CONFIG.presetPacks.get(switchEvent.previousPreset).clear();
+					CONFIG.presetPacks.get(switchEvent.previousPreset).addAll(resourcePacks);
+
+				}
+				if (CONFIG.presetPacks.containsKey(switchEvent.currentPreset) && !resourcePacks.equals(CONFIG.presetPacks.get(switchEvent.currentPreset)))
+				{
+					resourcePackManager.setEnabledProfiles(CONFIG.presetPacks.get(switchEvent.currentPreset));
+					MinecraftClient.getInstance().reloadResources();
+				}
+			}
 		});
 	}
 }
